22bb8e0ee70150cbf59c91c4aeeefd97dc7b8d23
Mostafa <noorahmadimustafa204@gmail.com>
2024-09-10 00:23:20 +0430

    Set up basic structure for product components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Provider } from "react-redux";
-
 import { store } from "./store/store";
+import { productData } from "./data/items";
+import ProductList from "./components/ProductList";
+import Product from "./components/Product";
 
 function App() {
   return (
     <Provider store={store}>
-      <div>App</div>
+      <ProductList>
+        {productData.map((item) => (
+          <div key={item.id}>
+            <Product {...item} />
+          </div>
+        ))}
+      </ProductList>
     </Provider>
   );
 }
